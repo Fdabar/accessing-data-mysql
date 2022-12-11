c42d7c62c811e02bf30bb57f0846bfc72ab871fa
Fdabar <dbarkauskas@gmail.com>
2022-12-11 20:01:45 +0200

    Load customer details with firstpage.html

--- src/main/java/lt/dalius/springdemo/controller/CustomerTemplatesController.java
+++ src/main/java/lt/dalius/springdemo/controller/CustomerTemplatesController.java
@@ -1,9 +1,12 @@
 package lt.dalius.springdemo.controller;
 
+import lt.dalius.springdemo.repository.model.Customer;
 import lt.dalius.springdemo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,5 +21,16 @@ public class CustomerTemplatesController {
     @GetMapping(path = "/test")
     public String getTestString(){
         return "test/html_test";
+    }
+
+    // http://localhost:8080/customer/firstpage/112
+    @GetMapping(path = "/firstpage/{custId}")
+    public String getCustomerForFirstPage(@PathVariable int custId, Model model){
+        Customer customer = customerService.getCustomerByCustomerNumber(custId);
+        model.addAttribute("company_name", customer.getCustomerName());
+        model.addAttribute("contact_last_name", customer.getContactLastName());
+        model.addAttribute("contact_first_name", customer.getContactFirstName());
+        model.addAttribute("country", customer.getCountry());
+        return "firstpage";
     }
 }
